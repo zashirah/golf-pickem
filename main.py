4ffa1bf0353079af474daaf4ba8cd31eb33f645e
Zach Shirah <zachshirah01@gmail.com>
2025-02-09 22:31:32 -0500

    added a tournament page and allowed for multiple db tables

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from fasthtml.common import *
 from dataclasses import dataclass
 
 
-def render(pick):
-    pid = f'pick-{pick.id}'
+db = database('data/golf_pickem.db')
+
+tournaments,picks = db.t.tournaments,db.t.picks
+
+if picks not in db.t:
+    picks.create(id=int, pickname=str, tier1_pick=str, tier2_pick=str, tier3_pick=str, tier4_pick=str, pk='id')
+if tournaments not in db.t:
+    tournaments.create(id=int, name=str, current=bool, allow_submissions=bool, pk='id')
+
+print(db.t)
+
+Tournament,Pick = tournaments.dataclass(),picks.dataclass()
+
+@patch
+def __ft__(self:Pick):
+    pid = f'pick-{self.id}'
     return Tr(
-        Td(pid),
-        Td(pick.id),
-        Td(pick.pickname), Td(pick.tier1_pick), Td(pick.tier2_pick), Td(pick.tier3_pick), Td(pick.tier4_pick), 
+        Td(self.pickname), Td(self.tier1_pick), Td(self.tier2_pick), Td(self.tier3_pick), Td(self.tier4_pick), 
         Td(
             Button(
                 'delete', 
-                hx_delete=f'/pick/{pick.id}', 
+                hx_delete=f'/picks/{self.id}', 
                 hx_swap='outerHTML', 
                 target_id=pid, 
                 style='background-color:red; border-color:red;'
@@ -20,48 +32,181 @@ def render(pick):
         id=pid
     )
 
-app,rt,picks,Pick = fast_app(
-    'picks.db', live=True, render=render,
-    id=int, pickname=str, tier1_pick=str, tier2_pick=str, tier3_pick=str, tier4_pick=str, pk='id')
+@patch
+def __ft__(self:Tournament):
+    tid = f'tournament-{self.id}'
+    return Tr(
+        Td(self.name), Td(self.current), Td(self.allow_submissions),
+        Td(
+            Button(
+                'delete', 
+                hx_delete=f'/tournaments/{self.id}', 
+                hx_swap='outerHTML', 
+                target_id=tid, 
+                style='background-color:red; border-color:red;'
+            )
+        ), 
+        Td(Button(
+                'Set Not Current' if self.current else 'Set Current', 
+                hx_patch=f'/tournaments/{self.id}/current', 
+                hx_swap='outerHTML', 
+                target_id=tid, 
+                style='background-color:green; border-color:green;',
+            )
+        ),
+        Td(Button(
+                'Disallow Submissions ' if self.current else 'Allow Submissions', 
+                hx_patch=f'/tournaments/{self.id}/submissions', 
+                hx_swap='outerHTML', 
+                target_id=tid, 
+                style='background-color:green; border-color:green;',
+            )
+        ),
+        id=tid
+    )
+
+app,rt = fast_app(live=True)
 
 """
 tables:
-* golfers
-    * name
-    * odds?
-    * tournament
-    * tier
-    * 1st round score 
-    * 2nd round score 
-    * 3rd round score 
-    * 4th round score 
-* players
-    * name
-    * groupme username
-* picks
-    * player name
-    * pick name (need this if a player has more than 1 pick - maybe auto-generate?)
-    * tier 1 pick
-    * tier 2 pick
-    * tier 3 pick
-    * tier 4 pick
-* tournaments
-    * name
-    * date
-    * location
+    * golfers
+        * name
+        * odds?
+        * tournament
+    * tournaments
+        * name
+        * date
+        * location
+    * tournament_golfer
+        * tournament
+        * tier
+        * round
+        * score
+        * hole
+    * users
+        * name
+        * groupme username
+    * picks
+        * player name
+        * pick name (need this if a player has more than 1 pick - maybe auto-generate?)
+        * tier 1 pick
+        * tier 2 pick
+        * tier 3 pick
+        * tier 4 pick
 """
 
-@dataclass
-class Pick:
-    pickname: str
-    tier1_pick: str
-    tier2_pick: str
-    tier3_pick: str
-    tier4_pick: str
+
+def options(tier:int):
+    return (
+        Option(f'Select a player from tier {tier}'),
+        Option(f'tier{tier}: option 1'),
+        Option(f'tier{tier}: option 2'),
+        Option(f'tier{tier}: option 3')
+    )
+
+def select_player(tier:int): return Select(options(tier), placeholder=f'Select tier {tier} player', name=f'tier{tier}_pick')
+
+def update_form(page=None):
+    if page == 'picks':
+        return Div(Form(
+            H3('Make your pick'),
+            Div(
+                Input(placeholder=f"Pick Name", name='pickname'),
+            ),
+            Grid(
+                select_player(1),
+                select_player(2),
+                select_player(3),
+                select_player(4)
+            ),
+            Button('Submit'),
+            hx_post="/picks",
+            hx_target="#picks-list",
+            hx_swap="beforeend",
+            hx_swap_oob='true'
+        ), hx_swap_oob='true', id='form')
+    elif page == 'tournaments':
+        return Div(Form(
+            H3('Create Tournament'),
+            Grid(
+                Input(placeholder="Tournment Name", name="name"),
+                Div(
+                    Label("Current", Input(type="Checkbox", name="current")),
+                    Label("Allow Submissions", Input(type="Checkbox", name="allow_submissions")),
+                ),
+            ),
+            Button('Submit'),
+            hx_post="/tournaments",
+            hx_target="#tournaments-list",
+            hx_swap="beforeend",
+            hx_swap_oob='true'
+        ), hx_swap_oob='true', id='form')
+    elif not page:
+        return Div(id='form')
+
+def get_header():
+    return Header(
+            Grid(
+                H1("Golf Pick'em"),
+                Button('Tournaments', hx_get='/tournaments', hx_swap_oob='true', hx_target="#home"),
+                Button("Pick'em", hx_get='/picks', hx_swap_oob='true', hx_target="#home"),
+            )
+        )
+
+@app.get('/')
+def home(): 
+    return Main(
+        get_header(),
+        update_form(),
+        Main(id='home')
+    )
+
+### TOURNAMENTS
+@app.get('/tournaments')
+def get_tournaments(): 
+    return Div(
+        Table(
+            Tr(Th('Tournament Name'), Th('Current'), Th('Allow Submissions'), Th(' '), Th(' ')),
+            *tournaments(), id='tournaments-list'
+        ),
+        id='home'
+    ), update_form(page='tournaments')
+
+def validate_tournament(tournament: Tournament): return True if tournament.name else False
+
+@app.post('/tournaments')
+def post_tournaments(tournament: Tournament): 
+    if validate_tournament(tournament):
+        return tournaments.insert(tournament)
+    
+@app.delete('/tournaments/{tid}')
+def delete_tournament(tid:int): tournaments.delete(tid)
+
+@app.patch('/tournaments/{tid}/current')
+def patch_tournament_current(tid:int): 
+    t = tournaments[tid]
+    t.current = not t.current
+    return tournaments.update(t)
+
+@app.patch('/tournaments/{tid}/submissions')
+def patch_tournament_submissions(tid:int): 
+    t = tournaments[tid]
+    t.allow_submissions = not t.allow_submissions
+    return tournaments.update(t)
+
+### PICKS 
+@app.get('/picks')
+def get_picks():
+    return Div(
+        Table(
+            Tr(Th('Pick Name'), Th('Tier 1'), Th('Tier 2'), Th('Tier 3'), Th('Tier 4'), Th(' ')),
+            *picks(), id='picks-list'
+        ),
+        id='home'
+    ), update_form('picks')
 
 def validate_pick(pick: Pick):
     errors = []
-    print(pick)
     if len(pick.pickname) < 1:
         errors.append('Must include username')
     if pick.tier1_pick == f'Select a player from tier 1':
@@ -73,54 +218,15 @@ def validate_pick(pick: Pick):
     if pick.tier4_pick == f'Select a player from tier 4':
         errors.append('Must include a pick from tier 4')
 
-    return errors
+    return False if errors else True
 
-def options(tier):
-    return (
-        Option(f'Select a player from tier {tier}'),
-        Option(f'tier{tier}: option 1'),
-        Option(f'tier{tier}: option 2'),
-        Option(f'tier{tier}: option 3')
-    )
+@app.post('/picks')
+def post_picks(pick: Pick): 
+    if validate_pick(pick):
+        return picks.insert(pick)
 
-def select_player(tier : int):
-    return Select(options(tier), placeholder=f'Select tier {tier} player', name=f'tier{tier}_pick')
+@app.delete('/picks/{pid}')
+def delete_pick(pid:int): picks.delete(pid)
 
-@app.get('/')
-def home(): 
-    return Title("Golf Pick'em"), Main(
-        H1("Golf Pick'em"),
-        Form(
-            Input(placeholder=f"Set pick name", name='pickname'),
-            Grid(
-                select_player(1),
-                select_player(2),
-                select_player(3),
-                select_player(4)
-            ),
-            Button('Submit'),
-            hx_post="/submit",
-            hx_target="#picks-list",
-            hx_swap="beforeend",
-            hx_swap_oob='true'
-        ),
-        Div(id="notes"),
-        Table(
-            Tr(Th('Pick Name'), Th('Tier 1'), Th('Tier 2'), Th('Tier 3'), Th('Tier 4'), Th('')),
-            *picks(), id='picks-list')
-    )
-
-@app.post('/submit')
-def submit(pick: Pick):
-    errors = validate_pick(pick)
-    if errors:
-        return Div(Ul(*[Li(error, style="color: red;") for error in errors]), id="notes")
-    
-    return picks.insert(pick) # , Div('Submitted successfully', id='notes', style="color: green;")
-
-@app.delete('/pick/{pid}')
-def delete_pick(pid:int): 
-    print(pid)
-    picks.delete(pid)
 
 serve()
